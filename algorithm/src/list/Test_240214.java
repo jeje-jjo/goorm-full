6fb1b65416fbfbd9829f19c57a1481c58aeb5a6d
hj-JJo <heyj.jjo@gmail.com>
2024-02-14 17:09:43 +0900

    20240214 algoritm

---
+++ algorithm/src/list/Test_240214.java
@@ -0,0 +1,15 @@
+package list;
+
+public class Test_240214 {
+
+    public int[] t01(int n, String[] words) {
+        int[] answer = {};
+
+        for(int i = 0; i < words.length; i++){
+
+            
+        }
+
+        return answer;
+    }
+}
