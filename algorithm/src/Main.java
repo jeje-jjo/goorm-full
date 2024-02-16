e534b26fe440dca8ca98b4081239695e0e3cf122
hj-JJo <heyj.jjo@gmail.com>
2024-02-16 14:18:07 +0900

    006 풀이완료

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,5 +1,6 @@
 import list.Test_240208;
 import list.Test_240213;
+import list.Test_240214;
 
 import java.util.Arrays;
 
@@ -7,13 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Test_240208 t01 = new Test_240208();
         Test_240213 t02 = new Test_240213();
+        Test_240214 t03 = new Test_240214();
 
         // t01.t11382();
         // t01.t25314();
         // t01.t10807();
         // t01.t0001();
 
-        System.out.println(Arrays.toString(t02.t03(24, 24)));
+        //System.out.println(Arrays.toString(t02.t03(24, 24)));
+        System.out.println(Arrays.toString(t03.t01(5, new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"})));
     }
 }
 
--- algorithm/src/list/Test_240214.java
+++ algorithm/src/list/Test_240214.java
@@ -1,13 +1,33 @@
 package list;
 
+import java.util.HashSet;
+
 public class Test_240214 {
 
     public int[] t01(int n, String[] words) {
-        int[] answer = {};
+        int[] answer = {0, 0};
+        HashSet<String> set = new HashSet<>();
 
         for(int i = 0; i < words.length; i++){
 
-            
+            // 첫번째 순서는 비교값이 없으므로 제외 && 전 순서의 마지막 글자와 현재 글자의 첫번째 순서가 다를 경우
+            if(i != 0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
+                // 탈락자의 번호는  n의 배수에서 나머지 순서이므로 나머지를 구하고, 인덱스는 0으로 시작하기 때문에 +1
+                // 탈락자의 순서는 n으로 나눈 몫에 +1
+                answer[0] = i% n + 1;
+                answer[1] = Math.round(((float) (i + 1) /n));
+                break;
+            }else{
+                set.add(words[i].toString());
+                if(set.size() != i+1){
+                    answer[0] = i% n + 1;
+                    answer[1] = Math.round(((float) (i + 1) /n));
+
+
+                    break;
+                }
+            }
+
         }
 
         return answer;
