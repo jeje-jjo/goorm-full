c93692d977d827581ae65eff1514b7ee3c4a7689
제제 <heyj.jjo@gmail.com>
2024-03-26 17:37:37 +0900

    nextjs / serverAction
    todo app create

---
+++ nextjs-server/src/app/components/TodoList.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+
+export type Todo = {
+  userId : number;
+  id : number;
+  title : string;
+  completed : boolean;
+}
+
+async function fetchTodos(){
+  try {
+    const res = await fetch('http://localhost:3001/todos');
+    const todos : Todo[] = await res.json();
+
+    return todos;
+
+  }catch (error) {
+    if(error instanceof Error){
+      console.log(error.stack);
+    }
+  }
+}
+
+const TodoList = async () => {
+  const todos = await fetchTodos();
+
+  let content;
+  if(!todos || todos.length === 0){
+    content = <p>Todo List is null</p>
+  } else {
+    const sortedTodos = todos.reverse();
+    content = (
+      <>
+        {sortedTodos.map((todo) => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+      </>
+      )
+  }
+  return content;
+}
+
+export default TodoList
